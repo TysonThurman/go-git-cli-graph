a21dd9b52ec5d91e208b8b4c1bb3725386210291
tysonthurman <tathurma@gmail.com>
2025-04-06 09:42:50 -0700

    added folder scans

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 // scan scans a new folder for Git repositories
@@ -17,6 +20,44 @@ func scan(folder string) {
 // stats generates a nice graph of your Git contributions
 func stats(email string) {
 	print("stats")
+}
+
+// scanGitFolders returns a list of subfolders of `folder` ending with `.git`.
+// Returns the base folder of the repo, the .git folder parent.
+// Recursively searches in the subfolders by passing an existing `folders` slice.
+func scanGitFolders(folders []string, folder string) []string {
+	// trim the last `/`
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+
+	return folders
 }
 
 func main() {
